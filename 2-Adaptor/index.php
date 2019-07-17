0b50567615b657172bee34f6abc8dfbe6783f468
lenovo <ahmad_wayfarer@yahoo.com>
2019-07-17 17:41:47 +0430

    Adaptor design pattern completed

--- 2-Adaptor/index.php
+++ 2-Adaptor/index.php
@@ -3,15 +3,20 @@
 require 'vendor/autoload.php';
 
 use Acme\Book;
+use Acme\Kindle;
 use Acme\BookInterface;
+use Acme\KindleAdaptor;
+use Acme\eReaderInterface;
 
 class Person
 {
-    public function read(BookInterface $book)
+//    public function read(BookInterface $book)
+    public function read(eReaderInterface $book)
     {
         $book->open();
         $book->turnPage();
     }
 }
 
-(new Person())->read(new Book);
+//(new Person())->read(new Book());
+(new Person())->read(new KindleAdaptor(new Kindle()));
--- 2-Adaptor/src/BookInterface.php
+++ 2-Adaptor/src/BookInterface.php
@@ -6,15 +6,11 @@ interface BookInterface
 {
     /**
      * Open the book
-     *
-     * @return void
      */
     public function open();
 
     /**
      * Turn the page
-     *
-     * @return void
      */
     public function turnPage();
 }
